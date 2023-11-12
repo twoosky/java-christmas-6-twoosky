43a8b20b85d08a2a738309cc066a6f13ac804e8e
twoosky <lhjae3@naver.com>
2023-11-13 01:56:39 +0900

    feat: 날짜 요일 변환 기능 구현

--- src/main/java/christmas/domain/VisitDate.java
+++ src/main/java/christmas/domain/VisitDate.java
@@ -1,8 +1,12 @@
 package christmas.domain;
 
 import christmas.utils.InvalidDateException;
+import java.time.DayOfWeek;
+import java.time.LocalDate;
 
 public class VisitDate {
+    private static final int YEAR = 2023;
+    public static final int MONTH = 12;
     private static final int MIN_DATE = 1;
     private static final int MAX_DATE = 31;
 
@@ -17,5 +21,10 @@ public class VisitDate {
         if (date < MIN_DATE || date > MAX_DATE) {
             throw new InvalidDateException();
         }
+    }
+
+    public DayOfWeek getDayOfWeek() {
+        LocalDate localDate = LocalDate.of(YEAR, MONTH, date);
+        return localDate.getDayOfWeek();
     }
 }
