d7090263475413692219ac4cce45099920b8bce0
twoosky <lhjae3@naver.com>
2023-11-13 02:22:53 +0900

    feat: 주문 개수가 숫자가 아닌 경우 예외 처리 구현

--- src/main/java/christmas/domain/Order.java
+++ src/main/java/christmas/domain/Order.java
@@ -1,11 +1,19 @@
 package christmas.domain;
 
+import static christmas.utils.ErrorMessages.INVALID_ORDER;
+
+import christmas.utils.IntegerConverter;
+
 public class Order {
     private final Menu menu;
     private final int quantity;
 
-    public Order(String menu, int quantity) {
+    public Order(String menu, String quantity) {
         this.menu = Menu.from(menu);
-        this.quantity = quantity;
+        this.quantity = convertType(quantity);
+    }
+
+    private int convertType(String quantity) {
+        return IntegerConverter.convert(quantity, INVALID_ORDER);
     }
 }
