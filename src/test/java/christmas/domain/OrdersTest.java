811f345aef9413f324b54499ea68d2a5eb6aa38e
twoosky <lhjae3@naver.com>
2023-11-13 04:28:05 +0900

    test: 주문 메뉴 입력에 대한 단위 테스트 작성

--- src/test/java/christmas/domain/OrdersTest.java
+++ src/test/java/christmas/domain/OrdersTest.java
@@ -15,41 +15,33 @@ import org.junit.jupiter.params.provider.ValueSource;
 public class OrdersTest {
 
     @ParameterizedTest
-    @ValueSource(strings = {"티본스테이크--1,바비큐립1", "티본스테이크:1,바비큐립-2"})
-    void 메뉴형식이_다른_경우_예외를_던진다(String inputOrders) {
-        Assertions.assertThatThrownBy(() -> new Orders(inputOrders))
+    @ValueSource(strings = {"티본스테이크--1,바비큐립1", "티본스테이크:1,바비큐립-2", "타파스-2초코케이크1"})
+    void 메뉴형식이_다른_경우_예외를_던진다(String 잘못된_메뉴형식_입력) {
+        Assertions.assertThatThrownBy(() -> new Orders(잘못된_메뉴형식_입력))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(INVALID_ORDER);
     }
 
-    @Test
-    void 중복메뉴를_입력한_경우_예외를_던진다() {
-        // given
-        String 중복_주문_입력 = "시저샐러드-1,시저샐러드-2";
-
-        // when & then
+    @ParameterizedTest
+    @ValueSource(strings = {"시저샐러드-1,시저샐러드-2", "제로콜라-3,양송이수프-1,양송이수프-2"})
+    void 중복메뉴를_입력한_경우_예외를_던진다(String 중복_주문_입력) {
         Assertions.assertThatThrownBy(() -> new Orders(중복_주문_입력))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(INVALID_ORDER);
     }
 
-    @Test
-    void 음료만_주문한_경우_예외를_던진다() {
-        // given
-        String 음료_주문_입력 = "제로콜라-2,레드와인-1";
-
-        // when & then
-        Assertions.assertThatThrownBy(() -> new Orders(음료_주문_입력))
+    @ParameterizedTest
+    @ValueSource(strings = {"레드와인-1,제로콜라-2", "제로콜라-3"})
+    void 음료만_주문한_경우_예외를_던진다(String 음료만_주문_입력) {
+        Assertions.assertThatThrownBy(() -> new Orders(음료만_주문_입력))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(INVALID_ORDER);
     }
 
     @Test
     void 초과_주문한_경우_예외를_던진다() {
-        // given
         String 초과_주문_입력 = "시저샐러드-4,티본스테이크-4,크리스마스파스타-3,제로콜라-6,아이스크림-4";
 
-        // when & then
         Assertions.assertThatThrownBy(() -> new Orders(초과_주문_입력))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(INVALID_ORDER);
