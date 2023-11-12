6fb4415ae8b94b774dae575ce5191cf7b672d85a
twoosky <lhjae3@naver.com>
2023-11-13 01:28:22 +0900

    test: 입력 받은 날짜에 대한 단위 테스트 작성

--- src/test/java/christmas/domain/VisitDateTest.java
+++ src/test/java/christmas/domain/VisitDateTest.java
@@ -1,18 +1,24 @@
 package christmas.domain;
 
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.Test;
+import static christmas.utils.ErrorMessages.INVALID_DATE;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+@DisplayName("VisitDate 클래스")
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 public class VisitDateTest {
 
-    @Test
-    void 범위에_벗어난_날짜가_입력되면_예외를_던진다() {
-        // given
-        int 입력_날짜 = 32;
-
-        // when & then
-        Assertions.assertThatThrownBy(() -> 날짜_생성_요청(입력_날짜))
+    @ParameterizedTest
+    @ValueSource(ints = {0, -1, 32})
+    void 범위에_벗어난_날짜가_입력되면_예외를_던진다(int inputDate) {
+        Assertions.assertThatThrownBy(() -> new VisitDate(inputDate))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+                .hasMessageContaining(INVALID_DATE);
     }
 }
