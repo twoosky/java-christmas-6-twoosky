89e0e5efba493ca6ce6585a875b04f16f16fda8e
twoosky <lhjae3@naver.com>
2023-11-13 03:10:46 +0900

    test: 메뉴형식이 다른 경우 단위 테스트 작성

--- src/test/java/christmas/domain/OrdersTest.java
+++ src/test/java/christmas/domain/OrdersTest.java
@@ -2,25 +2,21 @@ package christmas.domain;
 
 import static christmas.utils.ErrorMessages.INVALID_ORDER;
 
-import java.util.Arrays;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 @DisplayName("Orders 클래스")
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 public class OrdersTest {
 
-
-    @Test
-    void 메뉴형식이_다른_경우_예외를_던진다() {
-        // given
-        String 주문_메뉴 = "티본스테이크--1,바비큐립1";
-
-        // when & then
-        Assertions.assertThatThrownBy(() -> new 메뉴_주문_생성_요청(주문_메뉴))
+    @ParameterizedTest
+    @ValueSource(strings = {"티본스테이크--1,바비큐립1", "티본스테이크:1,바비큐립-2"})
+    void 메뉴형식이_다른_경우_예외를_던진다(String inputOrders) {
+        Assertions.assertThatThrownBy(() -> new Orders(inputOrders))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(INVALID_ORDER);
     }
