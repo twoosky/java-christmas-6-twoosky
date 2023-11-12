944bfd0fe0ea89e30f1521382856c3de93f6fcbd
twoosky <lhjae3@naver.com>
2023-11-13 02:35:24 +0900

    feat: 주문 개수가 1보다 작은 경우 예외 처리 구현

--- src/main/java/christmas/domain/Order.java
+++ src/main/java/christmas/domain/Order.java
@@ -3,17 +3,28 @@ package christmas.domain;
 import static christmas.utils.ErrorMessages.INVALID_ORDER;
 
 import christmas.utils.IntegerConverter;
+import christmas.utils.InvalidOrderException;
 
 public class Order {
+    public static final int MIN_ORDER_QUANTITY = 1;
     private final Menu menu;
     private final int quantity;
 
-    public Order(String menu, String quantity) {
-        this.menu = Menu.from(menu);
+    public Order(String menuName, String quantity) {
+        this.menu = Menu.from(menuName);
+
+        validateMinQuantity(quantity);
         this.quantity = convertType(quantity);
     }
 
     private int convertType(String quantity) {
         return IntegerConverter.convert(quantity, INVALID_ORDER);
+    }
+
+    private void validateMinQuantity(String value) {
+        int quantity = convertType(value);
+        if (quantity < MIN_ORDER_QUANTITY) {
+            throw new InvalidOrderException();
+        }
     }
 }
