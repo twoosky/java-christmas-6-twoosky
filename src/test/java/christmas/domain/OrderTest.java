6395bc71303e6380afdbd683c16a604713eb7948
twoosky <lhjae3@naver.com>
2023-11-13 02:21:08 +0900

    test: 주문 개수가 숫자가 아닌 경우 인수 테스트 작성

--- src/test/java/christmas/domain/OrderTest.java
+++ src/test/java/christmas/domain/OrderTest.java
@@ -15,9 +15,21 @@ public class OrderTest {
     @Test
     void 메뉴판에_없는_메뉴를_주문한_경우_예외를_던진다() {
         String inputMenu = "곱창";
-        int orderQuantity = 1;
+        String orderQuantity = "1";
 
         Assertions.assertThatThrownBy(() -> new Order(inputMenu, orderQuantity))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(INVALID_ORDER);
+    }
+
+    @Test
+    void 메뉴의_주문개수가_숫자가_아닌_경우_예외를_던진다() {
+        // given
+        String 입력_메뉴 = "곱창";
+        String 주문_개수 = "a";
+
+        // when & then
+        Assertions.assertThatThrownBy(() -> new Order(입력_메뉴, 주문_개수))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(INVALID_ORDER);
     }
