25014718ee2132b5d7398a69d00a7d4d1b318f0b
twoosky <lhjae3@naver.com>
2023-11-13 04:03:48 +0900

    feat: 메뉴 초과 검증 기능 구현

--- src/main/java/christmas/domain/Orders.java
+++ src/main/java/christmas/domain/Orders.java
@@ -1,14 +1,25 @@
 package christmas.domain;
 
+import static christmas.utils.ErrorMessages.INVALID_ORDER;
+
+import christmas.utils.IntegerConverter;
 import christmas.utils.InvalidOrderException;
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
 
 public class Orders {
     public static final String ORDER_DELIMITER = ",";
     public static final String MENU_AND_QUANTITY_DELIMITER = "-";
+    public static final int MAX_ORDER_QUANTITY = 20;
+
+    private final List<Order> orders;
 
     public Orders(String orders) {
         validateFormat(orders);
+        this.orders = createOrders(orders);
+
+        validateOverQuantity();
     }
 
     private void validateFormat(String value) {
@@ -17,10 +28,31 @@ public class Orders {
         }
     }
 
+    private Stream<String[]> getOrdersStream(String value) {
+        return Arrays.stream(value.split(ORDER_DELIMITER))
+                .map(order -> order.split(MENU_AND_QUANTITY_DELIMITER));
+    }
+
     private boolean isInvalidFormat(String value) {
-        String[] orders = value.split(ORDER_DELIMITER);
-        return Arrays.stream(orders)
-                .map(order -> order.split(MENU_AND_QUANTITY_DELIMITER))
-                .anyMatch(split -> split.length != 2);
+        return getOrdersStream(value)
+                .anyMatch(order -> order.length != 2);
+    }
+
+    private List<Order> createOrders(String value) {
+        return getOrdersStream(value)
+                .map(order -> new Order(order[0], order[1]))
+                .toList();
+    }
+
+    private void validateOverQuantity() {
+        if (sumOrderQuantity() > MAX_ORDER_QUANTITY) {
+            throw new InvalidOrderException();
+        }
+    }
+
+    private int sumOrderQuantity() {
+        return orders.stream()
+                .mapToInt(Order::getQuantity)
+                .sum();
     }
 }
