1b1dabf205ae4359b96f60f7e1a84f3e44f5bcf4
twoosky <lhjae3@naver.com>
2023-11-13 23:44:37 +0900

    test: 가격에 대한 증정품 제공 기능 단위 테스트 작성

--- src/test/java/christmas/domain/PriceGiftPolicyTest.java
+++ src/test/java/christmas/domain/PriceGiftPolicyTest.java
@@ -1,30 +1,27 @@
 package christmas.domain;
 
+import christmas.domain.policy.GiftPolicy;
+import christmas.domain.policy.PriceGiftPolicy;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 @DisplayName("PriceGiftPolicy 클래스")
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 public class PriceGiftPolicyTest {
 
-    /**
-     * GIVEN 티본스테이크(55,000) 2개와 시저샐러드(8,000) 2개를 주문한다.
-     * WHEN  가격에 대한 증정품 제공 여부를 계산한다.
-     * THEN  증정품을 제공한다.
-     */
-    @Test
-    void 할인_전_총주문_금액이_12만원_이상이면_증정품을_제공한다() {
-        // given
-        String 주문 = "티본스테이크-2,시저샐러드-2";
-        Orders 주문_목록 = new Orders(주문);
+    @ParameterizedTest
+    @ValueSource(strings = {"티본스테이크-2,시저샐러드-2", "시저샐러드-1,바비큐립-2,크리스마스파스타-2,제로콜라-3"})
+    void 할인_전_총주문_금액이_12만원_이상이면_증정품을_제공한다(String 주문) {
+        Orders orders = new Orders(주문);
+        GiftPolicy giftPolicy = new PriceGiftPolicy();
 
-        // when
-        증정품_종류 증정품 = 가격에_대한_증정품_정책.증정품_제공(주문_목록);
+        GiftType 증정품 = giftPolicy.give(orders);
 
-        // then
-        Assertions.assertEquals(증정품_종류, 샴페인);
+        Assertions.assertEquals(증정품, GiftType.CHAMPAGNE);
     }
 }
