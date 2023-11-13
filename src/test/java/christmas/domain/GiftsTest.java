c76ab9d3b15a6fd5cb1d90e2d3bc1abbaae3f430
twoosky <lhjae3@naver.com>
2023-11-14 02:52:27 +0900

    test: 증정 금액 총합 계산 단위 테스트 구현

--- src/test/java/christmas/domain/GiftsTest.java
+++ src/test/java/christmas/domain/GiftsTest.java
@@ -2,38 +2,28 @@ package christmas.domain;
 
 import static christmas.domain.GiftType.CHAMPAGNE;
 
-import christmas.domain.policy.gift.GiftPolicy;
 import christmas.domain.policy.gift.PriceGiftPolicy;
 import java.util.List;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 @DisplayName("Gifts 클래스")
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 public class GiftsTest {
 
-    /**
-     * GIVEN 방문 날짜와 주문을 입력한다.
-     *       증정 정책에 따른 증정 객체 리스트를 생성한다.
-     * WHEN  각 증정 정책의 증정품 가격을 합한다.
-     * THEN  증정품 가격의 총합을 비교한다.
-     */
-    @Test
-    void 총_증정_금액을_계산한다() {
-        // given
-        String 주문 = "티본스테이크-2,레드와인-1,초코케이크-2,제로콜라-1";
-        Orders 주문_목록 = new Orders(주문);
-        List<Gift> 증정_목록 = 증정정책에_따른_증정객체_리스트_생성();
-        증정_목록_객체_생성(증정_목록);
+    @ParameterizedTest
+    @CsvSource(value= {"티본스테이크-2,시저샐러드-2:25000", "티본스테이크-1,시저샐러드-2:0"}, delimiter = ':')
+    void 총_증정_금액을_계산한다(String 주문, int 예상_증정금액_총합) {
+        Orders orders = new Orders(주문);
+        Gifts gifts = new Gifts(증정정책에_따른_증정객체_리스트_생성());
 
-        // when
-        int 증정금액_총합 = 증정_목록_객체.총합_계산(주문_목록);
+        int 증정금액_총합 = gifts.calculateTotalPrice(orders);
 
-        // then
-        Assertions.assertEquals(증정금액_총합, 25000);
+        Assertions.assertEquals(증정금액_총합, 예상_증정금액_총합);
     }
 
     private static List<Gift> 증정정책에_따른_증정객체_리스트_생성() {
