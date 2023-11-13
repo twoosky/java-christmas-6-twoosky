3ea7bfa7301e5574ae2ef10eb3b91695ade26b4d
twoosky <lhjae3@naver.com>
2023-11-13 20:53:02 +0900

    test: 크리스마스 디데이 할인 예외 인수 테스트 작성

--- src/test/java/christmas/domain/DdayDiscountPolicyTest.java
+++ src/test/java/christmas/domain/DdayDiscountPolicyTest.java
@@ -28,4 +28,23 @@ public class DdayDiscountPolicyTest {
         // then
         Assertions.assertEquals(할인_금액, 3400);
     }
+
+    /**
+     * GIVEN 크리스마스 이후 방문한다.
+     * WHEN  크리스마스 디데이 할인 금액을 계산한다.
+     * THEN  0원 할인된다.
+     */
+    @Test
+    void 크리스마스_이후_방문한_경우_할인되지_않는다() {
+        // given
+        String 주문 = "티본스테이크-2,레드와인-1,초코케이크-2";
+        VisitDate 방문_날짜 = new VisitDate(31);
+        Orders 주문_목록 = new Orders(주문);
+
+        // when
+        int 할인_금액 = 크리스마스_디데이_할인정책.할인(방문_날짜, 주문_목록);
+
+        // then
+        Assertions.assertEquals(할인_금액, 3400);
+    }
 }
