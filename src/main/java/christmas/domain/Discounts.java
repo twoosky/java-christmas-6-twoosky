261c3b6db1619d8798c5407f106dcb8010605eb2
twoosky <lhjae3@naver.com>
2023-11-14 03:05:48 +0900

    feat: 할인 최소 주문 금액 조건 검증 기능 구현

--- src/main/java/christmas/domain/Discounts.java
+++ src/main/java/christmas/domain/Discounts.java
@@ -3,6 +3,9 @@ package christmas.domain;
 import java.util.List;
 
 public class Discounts {
+    public static final int MINIMUM_ORDERS_PRICE = 10000;
+    public static final int NONE_DISCOUNT = 0;
+
     private final List<Discount> discounts;
 
     public Discounts(List<Discount> discounts) {
@@ -10,6 +13,17 @@ public class Discounts {
     }
 
     public int calculateTotalDiscount(VisitDate visitDate, Orders orders) {
+        if (canDiscount(orders)) {
+            return sumDiscount(visitDate, orders);
+        }
+        return NONE_DISCOUNT;
+    }
+
+    private boolean canDiscount(Orders orders) {
+        return orders.calculateTotalPrice() >= MINIMUM_ORDERS_PRICE;
+    }
+
+    private int sumDiscount(VisitDate visitDate, Orders orders) {
         return discounts.stream()
                 .mapToInt(discount -> discount.discount(visitDate, orders))
                 .sum();
