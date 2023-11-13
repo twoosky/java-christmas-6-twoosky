7e22b3feca11f824fad73f43dc384ebb065c8295
twoosky <lhjae3@naver.com>
2023-11-13 21:16:54 +0900

    test: 크리스마스 디데이 할인 기능 단위 테스트 작성

--- src/test/java/christmas/domain/DdayDiscountPolicyTest.java
+++ src/test/java/christmas/domain/DdayDiscountPolicyTest.java
@@ -1,10 +1,12 @@
 package christmas.domain;
 
+import christmas.domain.policy.DdayDiscountPolicy;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 @DisplayName("DdayDiscountPolicy 클래스")
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
@@ -15,36 +17,29 @@ public class DdayDiscountPolicyTest {
      * WHEN  크리스마스 디데이 할인 금액을 계산한다.
      * THEN  3400원 할인된다.
      */
-    @Test
-    void 크리스마스_디데이만큼_100원씩_추가할인한다() {
-        // given
+    @ParameterizedTest
+    @CsvSource(value = {"25:3400", "1:1000", "15:2400"}, delimiter = ':')
+    void 크리스마스_디데이만큼_100원씩_추가_할인한다(int 방문_날짜, int 예상_할인_금액) {
         String 주문 = "티본스테이크-2,레드와인-1,초코케이크-2";
-        VisitDate 방문_날짜 = new VisitDate(25);
-        Orders 주문_목록 = new Orders(주문);
+        VisitDate visitDate = new VisitDate(방문_날짜);
+        Orders orders = new Orders(주문);
+        DdayDiscountPolicy ddayDiscountPolicy = new DdayDiscountPolicy();
 
-        // when
-        int 할인_금액 = 크리스마스_디데이_할인정책.할인(방문_날짜, 주문_목록);
+        int 할인_금액 = ddayDiscountPolicy.discount(visitDate, orders);
 
-        // then
-        Assertions.assertEquals(할인_금액, 3400);
+        Assertions.assertEquals(할인_금액, 예상_할인_금액);
     }
 
-    /**
-     * GIVEN 크리스마스 이후 방문한다.
-     * WHEN  크리스마스 디데이 할인 금액을 계산한다.
-     * THEN  0원 할인된다.
-     */
-    @Test
-    void 크리스마스_이후_방문한_경우_할인되지_않는다() {
-        // given
+    @ParameterizedTest
+    @CsvSource(value = {"31:0", "26:0"}, delimiter = ':')
+    void 크리스마스_이후에_방문하는_경우_할인하지_않는다(int 방문_날짜, int 예상_할인_금액) {
         String 주문 = "티본스테이크-2,레드와인-1,초코케이크-2";
-        VisitDate 방문_날짜 = new VisitDate(31);
-        Orders 주문_목록 = new Orders(주문);
+        VisitDate visitDate = new VisitDate(방문_날짜);
+        Orders orders = new Orders(주문);
+        DdayDiscountPolicy ddayDiscountPolicy = new DdayDiscountPolicy();
 
-        // when
-        int 할인_금액 = 크리스마스_디데이_할인정책.할인(방문_날짜, 주문_목록);
+        int 할인_금액 = ddayDiscountPolicy.discount(visitDate, orders);
 
-        // then
-        Assertions.assertEquals(할인_금액, 3400);
+        Assertions.assertEquals(할인_금액, 예상_할인_금액);
     }
 }
