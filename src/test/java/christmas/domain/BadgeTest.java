f363f45cbacfbbbfdbacf34af9ac3e36eb0d6da7
twoosky <lhjae3@naver.com>
2023-11-14 03:44:25 +0900

    test: 혜택 금액에 따른 배지 부여 기능 인수 테스트 작성

--- src/test/java/christmas/domain/BadgeTest.java
+++ src/test/java/christmas/domain/BadgeTest.java
@@ -1,11 +1,30 @@
 package christmas.domain;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
-@DisplayName("Badge 클래스")
+@DisplayName("Badge Enum")
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 public class BadgeTest {
+
+    /**
+     * GIVEN 혜택 금액을 정의한다.
+     * WHEN  혜택 금액에 따라 배지를 부여한다.
+     * THEN  배지를 비교한다.
+     */
+    @ParameterizedTest
+    @CsvSource(value = {"1000:없음", "5000:별", "10000:트리", "20000:산타"}, delimiter = ':')
+    void 혜택금액에_따라_배지이름을_반환한다(int 혜택금액, String 예상_배지_이름) {
+        // when
+        String 배지_이름 = 배지.혜택금액에_따른_배지이름_반환();
+
+        // then
+        Assertions.assertEquals(배지_이름, 예상_배지_이름);
+    }
 }
