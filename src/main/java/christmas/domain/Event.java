362a8859ae4ee64ba71c378f19cc010f7ad188ab
twoosky <lhjae3@naver.com>
2023-11-14 04:48:49 +0900

    feat: 할인 후 예상 결제 금액 계산 기능 구현

--- src/main/java/christmas/domain/Event.java
+++ src/main/java/christmas/domain/Event.java
@@ -15,11 +15,15 @@ public class Event {
     }
 
     public int getTotalDiscount(VisitDate visitDate, Orders orders) {
-        return calculateAmount(() -> discounts.sumDiscount(visitDate, orders), orders);
+        return getAmount(() -> discounts.sumDiscount(visitDate, orders), orders);
     }
 
     public int getTotalBenefit(VisitDate visitDate, Orders orders) {
-        return calculateAmount(() -> sumBenefit(visitDate, orders), orders);
+        return getAmount(() -> sumBenefit(visitDate, orders), orders);
+    }
+
+    public int getPaymentAfterDiscount(VisitDate visitDate, Orders orders) {
+        return orders.getTotalPrice() - discounts.sumDiscount(visitDate, orders);
     }
 
     private int sumBenefit(VisitDate visitDate, Orders orders) {
@@ -27,10 +31,10 @@ public class Event {
     }
 
     private boolean isEventApplicable(Orders orders) {
-        return orders.calculateTotalPrice() >= MINIMUM_ORDERS_PRICE;
+        return orders.getTotalPrice() >= MINIMUM_ORDERS_PRICE;
     }
 
-    private <T> int calculateAmount(Supplier<T> supplier, Orders orders) {
+    private <T> int getAmount(Supplier<T> supplier, Orders orders) {
         if (isEventApplicable(orders)) {
             return (int) supplier.get();
         }
