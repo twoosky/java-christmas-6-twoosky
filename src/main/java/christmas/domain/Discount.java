7cbc2bc5d34f7b2d5dc6af028e92087430f82570
twoosky <lhjae3@naver.com>
2023-11-14 00:24:05 +0900

    feat: 각 할인 정책에 대한 할인 계산 기능 구현

---
+++ src/main/java/christmas/domain/Discount.java
@@ -0,0 +1,17 @@
+package christmas.domain;
+
+import christmas.domain.policy.DiscountPolicy;
+
+public class Discount {
+    private final DiscountType type;
+    private final DiscountPolicy policy;
+
+    public Discount(DiscountType type, DiscountPolicy policy) {
+        this.type = type;
+        this.policy = policy;
+    }
+
+    public int discount(VisitDate visitDate, Orders orders) {
+        return policy.discount(visitDate, orders);
+    }
+}
