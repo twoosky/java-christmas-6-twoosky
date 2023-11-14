0dc262f0b0e399e8927882cbbacbd2868800a31a
twoosky <lhjae3@naver.com>
2023-11-14 18:22:56 +0900

    feat: 할인/증정 정책 주입 및 컨트롤러 생성

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,7 +1,50 @@
 package christmas;
 
+import static christmas.domain.DiscountType.CHRISTMAS_DDAY;
+import static christmas.domain.DiscountType.SPECIAL;
+import static christmas.domain.DiscountType.WEEKDAY;
+import static christmas.domain.DiscountType.WEEKEND;
+import static christmas.domain.GiftType.CHAMPAGNE;
+
+import christmas.controller.EventController;
+import christmas.domain.DiscountPolicies;
+import christmas.domain.DiscountType;
+import christmas.domain.GiftPolicies;
+import christmas.domain.GiftType;
+import christmas.domain.Restaurant;
+import christmas.domain.policy.discount.DdayDiscountPolicy;
+import christmas.domain.policy.discount.DiscountPolicy;
+import christmas.domain.policy.discount.SpecialDiscountPolicy;
+import christmas.domain.policy.discount.WeekdayDiscountPolicy;
+import christmas.domain.policy.discount.WeekendDiscountPolicy;
+import christmas.domain.policy.gift.GiftPolicy;
+import christmas.domain.policy.gift.PriceGiftPolicy;
+import christmas.view.InputView;
+import christmas.view.OutputView;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Restaurant restaurant = new Restaurant(createDiscountPolicies(), createGiftPolicies());
+        EventController controller = new EventController(restaurant, new InputView(), new OutputView());
+        controller.run();
+    }
+
+    private static DiscountPolicies createDiscountPolicies() {
+        Map<DiscountType, DiscountPolicy> policies = new HashMap<>();
+        policies.put(CHRISTMAS_DDAY, new DdayDiscountPolicy());
+        policies.put(WEEKDAY, new WeekdayDiscountPolicy());
+        policies.put(WEEKEND, new WeekendDiscountPolicy());
+        policies.put(SPECIAL, new SpecialDiscountPolicy());
+
+        return new DiscountPolicies(policies);
+    }
+
+    private static GiftPolicies createGiftPolicies() {
+        Map<GiftType, GiftPolicy> policies = new HashMap<>();
+        policies.put(CHAMPAGNE, new PriceGiftPolicy());
+
+        return new GiftPolicies(policies);
     }
 }
