ae2ad2baddb5a38367d457687657f657452ea6b8
twoosky <lhjae3@naver.com>
2023-11-14 15:47:09 +0900

    refactor: 할인, 증정 내역 영수증 객체로 통합

--- src/main/java/christmas/domain/Bill.java
+++ src/main/java/christmas/domain/Bill.java
@@ -1,38 +1,38 @@
 package christmas.domain;
 
-import christmas.dto.EventsDto;
+import christmas.dto.BillDto;
 import java.util.function.Supplier;
 
-public class Event {
+public class Bill {
     public static final int MINIMUM_ORDERS_PRICE = 10000;
     public static final int EVENT_NOT_APPLIED_AMOUNT = 0;
 
     private final Discounts discounts;
     private final Gifts gifts;
 
-    public Event(Discounts discounts, Gifts gifts) {
+    public Bill(Discounts discounts, Gifts gifts) {
         this.discounts = discounts;
         this.gifts = gifts;
     }
 
-    public int getTotalDiscount(VisitDate visitDate, Orders orders) {
-        return getAmount(() -> discounts.sumDiscount(visitDate, orders), orders);
+    public int getTotalDiscount(Orders orders) {
+        return getAmount(discounts::sumDiscount, orders);
     }
 
-    public int getTotalBenefit(VisitDate visitDate, Orders orders) {
-        return getAmount(() -> sumBenefit(visitDate, orders), orders);
+    public int getTotalBenefit(Orders orders) {
+        return getAmount(this::sumBenefit, orders);
     }
 
-    public int getPaymentAfterDiscount(VisitDate visitDate, Orders orders) {
-        return orders.getTotalPrice() - discounts.sumDiscount(visitDate, orders);
+    public int getPaymentAfterDiscount(Orders orders) {
+        return orders.getTotalPrice() - discounts.sumDiscount();
     }
 
-    public EventsDto getDiscountsResult() {
-
+    public BillDto toDto() {
+        return new BillDto(discounts.getResult(), gifts.getResult());
     }
 
-    private int sumBenefit(VisitDate visitDate, Orders orders) {
-        return discounts.sumDiscount(visitDate, orders) + gifts.sumPrice(orders);
+    private int sumBenefit() {
+        return discounts.sumDiscount() + gifts.sumPrice();
     }
 
     private boolean isEventApplicable(Orders orders) {
--- src/test/java/christmas/domain/BillTest.java
+++ src/test/java/christmas/domain/BillTest.java
@@ -6,88 +6,79 @@ import static christmas.domain.DiscountType.WEEKDAY;
 import static christmas.domain.DiscountType.WEEKEND;
 import static christmas.domain.GiftType.CHAMPAGNE;
 
-import christmas.domain.policy.discount.DdayDiscountPolicy;
-import christmas.domain.policy.discount.SpecialDiscountPolicy;
-import christmas.domain.policy.discount.WeekdayDiscountPolicy;
-import christmas.domain.policy.discount.WeekendDiscountPolicy;
-import christmas.domain.policy.gift.PriceGiftPolicy;
 import java.util.List;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 @DisplayName("Event 클래스")
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
-public class EventTest {
-    private static Event event;
+public class BillTest {
+    private static Bill bill;
 
     @BeforeEach
     void setUp() {
-        Discounts discounts = new Discounts(할인정책에_따른_할인객체_리스트_생성());
-        Gifts gifts = new Gifts(증정정책에_따른_증정객체_리스트_생성());
-        event = new Event(discounts, gifts);
+        Discounts discounts = new Discounts(할인객체_리스트_생성());
+        Gifts gifts = new Gifts(증정객체_리스트_생성());
+        bill = new Bill(discounts, gifts);
     }
 
-    private List<Discount> 할인정책에_따른_할인객체_리스트_생성() {
+    private List<Discount> 할인객체_리스트_생성() {
         return List.of(
-                new Discount(CHRISTMAS_DDAY, new DdayDiscountPolicy()),
-                new Discount(SPECIAL, new SpecialDiscountPolicy()),
-                new Discount(WEEKDAY, new WeekdayDiscountPolicy()),
-                new Discount(WEEKEND, new WeekendDiscountPolicy())
+                new Discount(CHRISTMAS_DDAY, 3400),
+                new Discount(SPECIAL, 1000),
+                new Discount(WEEKDAY, 4046),
+                new Discount(WEEKEND, 0)
         );
     }
 
-    private static List<Gift> 증정정책에_따른_증정객체_리스트_생성() {
+    private static List<Gift> 증정객체_리스트_생성() {
         return List.of(
-                new Gift(CHAMPAGNE, new PriceGiftPolicy())
+                new Gift(CHAMPAGNE, 1)
         );
     }
 
-    @ParameterizedTest
-    @CsvSource(value = {"티본스테이크-2,레드와인-1,초코케이크-2:10:31946", "티본스테이크-1,바비큐립-1,해산물파스타-1:30:31069"}, delimiter = ':')
-    void 모든_이벤트에_대한_혜택_금액_총합을_계산한다(String 주문, int 방문_날짜, int 에상_혜택금액_총합) {
-        VisitDate visitDate = new VisitDate(방문_날짜);
+    @Test
+    void 모든_이벤트에_대한_혜택_금액_총합을_계산한다() {
+        String 주문 = "티본스테이크-2,레드와인-1,초코케이크-2,제로콜라-1";
         Orders orders = new Orders(주문);
 
-        int 혜택금액_총합 = event.getTotalBenefit(visitDate, orders);
+        int 혜택금액_총합 = bill.getTotalBenefit(orders);
 
-        Assertions.assertEquals(혜택금액_총합, 에상_혜택금액_총합);
+        Assertions.assertEquals(혜택금액_총합, 33_446);
     }
 
-    @ParameterizedTest
-    @CsvSource(value = {"티본스테이크-2,레드와인-1,초코케이크-2:10:6946", "티본스테이크-1,바비큐립-1,해산물파스타-1:30:6069"}, delimiter = ':')
-    void 할인_이벤트에_대한_할인_금액_합계를_계산한다(String 주문, int 방문_날짜, int 에상_혜택금액_총합) {
-        VisitDate visitDate = new VisitDate(방문_날짜);
+    @Test
+    void 할인_이벤트에_대한_할인_금액_합계를_계산한다() {
+        String 주문 = "티본스테이크-2,레드와인-1,초코케이크-2,제로콜라-1";
         Orders orders = new Orders(주문);
 
-        int 혜택금액_총합 = event.getTotalDiscount(visitDate, orders);
+        int 혜택금액_총합 = bill.getTotalDiscount(orders);
 
-        Assertions.assertEquals(혜택금액_총합, 에상_혜택금액_총합);
+        Assertions.assertEquals(혜택금액_총합, 8_446);
     }
 
-    @ParameterizedTest
-    @CsvSource(value = {"티본스테이크-2,레드와인-1:9:164154", "바비큐립-1,시저샐러드-1,아이스크림-2:25:63554"}, delimiter = ':')
-    void 할인_후_예상_결제금액을_계산한다(String 주문, int 방문_날짜, int 에상_결제금액) {
-        VisitDate visitDate = new VisitDate(방문_날짜);
+    @Test
+    void 할인_후_예상_결제금액을_계산한다() {
+        String 주문 = "티본스테이크-2,레드와인-1,초코케이크-2,제로콜라-1";
         Orders orders = new Orders(주문);
 
-        int 혜택금액_총합 = event.getPaymentAfterDiscount(visitDate, orders);
+        int 혜택금액_총합 = bill.getPaymentAfterDiscount(orders);
 
-        Assertions.assertEquals(혜택금액_총합, 에상_결제금액);
+        Assertions.assertEquals(혜택금액_총합, 194_554);
     }
 
     @ParameterizedTest
     @ValueSource(strings = {"타파스-1,제로콜라-1", "제로콜라-1,아이스크림-1", "시저샐러드-1"})
     void 총_주문금액이_10000원보다_작으면_혜택_금액으로_0을_반환한다(String 주문) {
-        VisitDate visitDate = new VisitDate(25);
         Orders orders = new Orders(주문);
 
-        int 혜택금액_총합 = event.getTotalBenefit(visitDate, orders);
+        int 혜택금액_총합 = bill.getTotalBenefit(orders);
 
         Assertions.assertEquals(혜택금액_총합, 0);
     }
@@ -95,10 +86,9 @@ public class EventTest {
     @ParameterizedTest
     @ValueSource(strings = {"타파스-1,제로콜라-1", "제로콜라-1,아이스크림-1", "시저샐러드-1"})
     void 총_주문금액이_10000원보다_작으면_할인_금액으로_0을_반환한다(String 주문) {
-        VisitDate visitDate = new VisitDate(25);
         Orders orders = new Orders(주문);
 
-        int 혜택금액_총합 = event.getTotalDiscount(visitDate, orders);
+        int 혜택금액_총합 = bill.getTotalDiscount(orders);
 
         Assertions.assertEquals(혜택금액_총합, 0);
     }
