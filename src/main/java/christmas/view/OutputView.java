2305067f386b34451ed007bd8106aa6f120baacc
twoosky <lhjae3@naver.com>
2023-11-14 18:21:08 +0900

    feat: 할인 결제 금액 및 배지 출력 기능 구현

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -1,70 +1,86 @@
 package christmas.view;
 
-import christmas.domain.Menu;
 import christmas.dto.BenefitDto;
 import christmas.dto.GiftsDto;
 import christmas.dto.OrdersDto;
-import java.util.Map.Entry;
+import java.util.Map;
 
 public class OutputView {
     public static final String OUTPUT_RESULT = "12월 3일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!";
-    public static final String OUTPUT_ORDER_MENU_LIST = "%n<주문 메뉴>";
-    public static final String OUTPUT_ORDER_MENU = "%s %d개";
-    public static final String OUTPUT_TOTAL_ORDER_PRICE = "%n<할인 전 총주문 금액>";
-    public static final String OUTPUT_GIFT_LIST = "%n<증정 메뉴>";
-    public static final String OUTPUT_GIFT = "%s %d개";
-    public static final String OUTPUT_BENEFIT_LIST = "%n<혜택 내역>";
-    public static final String OUTPUT_BENEFIT = "%s: -%,d원";
+    public static final String OUTPUT_ORDER_MENU_LIST = "%n<주문 메뉴>%n";
+    public static final String OUTPUT_ORDER_MENU = "%s %d개%n";
+    public static final String OUTPUT_TOTAL_ORDER_PRICE = "%n<할인 전 총주문 금액>%n";
+    public static final String OUTPUT_TOTAL_ORDER_PRICE_FORMAT = "%,d원%n";
+    public static final String OUTPUT_GIFT_LIST = "%n<증정 메뉴>%n";
+    public static final String OUTPUT_GIFT_FORMAT = "%s %d개%n";
+    public static final String OUTPUT_BENEFIT_LIST = "%n<혜택 내역>%n";
+    public static final String OUTPUT_BENEFIT_FORMAT = "%s: -%,d원%n";
     public static final String OUTPUT_GIFT_BENEFIT = "증정 이벤트";
-    public static final String OUTPUT_TOTAL_BENEFIT = "%n<총혜택 금액>";
-    public static final String OUTPUT_BADGE = "%n<12월 이벤트 배지>";
+    public static final String OUTPUT_TOTAL_BENEFIT = "%n<총혜택 금액>%n";
+    public static final String OUTPUT_TOTAL_BENEFIT_FORMAT = "-%,d원%n";
+    public static final String OUTPUT_DISCOUNT_PRICE = "%n<할인 후 예상 결제 금액>%n";
+    public static final String OUTPUT_DISCOUNT_PRICE_FORMAT = "%,d원%n";
+    public static final String OUTPUT_BADGE = "%n<12월 이벤트 배지>%n";
+    public static final String OUTPUT_NOTHING = "없음";
 
     public void printOrders(OrdersDto dto) {
+        System.out.println(OUTPUT_RESULT);
         System.out.printf(OUTPUT_ORDER_MENU_LIST);
-        dto.orders().entrySet().forEach(this::printOrder);
+        dto.orders().forEach(this::printOrder);
     }
 
-    private void printOrder(Entry<String, Integer> order) {
-        System.out.printf(OUTPUT_ORDER_MENU, order.getKey(), order.getValue());
+    private void printOrder(String menu, int quantity) {
+        System.out.printf(OUTPUT_ORDER_MENU, menu, quantity);
     }
 
     public void printPrice(int price) {
         System.out.printf(OUTPUT_TOTAL_ORDER_PRICE);
-        System.out.println(price);
+        System.out.printf(OUTPUT_TOTAL_ORDER_PRICE_FORMAT, price);
     }
 
     public void printGifts(GiftsDto dto) {
         System.out.printf(OUTPUT_GIFT_LIST);
-        dto.gifts().entrySet().forEach(this::printGift);
+        print(() -> dto.gifts().forEach(this::printGift), dto.gifts());
     }
 
-    private void printGift(Entry<String, Integer> gift) {
-        System.out.printf(OUTPUT_GIFT, gift.getKey(), gift.getValue());
+    private void printGift(String gift, Integer quantity) {
+        System.out.printf(OUTPUT_GIFT_FORMAT, gift, quantity);
     }
 
     public void printBenefit(BenefitDto dto) {
         System.out.printf(OUTPUT_BENEFIT_LIST);
-        dto.discounts().forEach(this::printDiscount);
-
-        System.out.printf(OUTPUT_BENEFIT, OUTPUT_GIFT_BENEFIT, dto.giftPrice());
+        print(() -> dto.discounts().forEach(this::printDiscount), dto.discounts());
+        printGiftBenefit(dto.giftPrice());
     }
 
-    // !!!!!!!!!!!!! 이게 되면 entrySet 쓰지 말자 그리고, 아래 메소드 함수형 인터페이스 사용해서 바꾸자
+    private void printGiftBenefit(int giftPrice) {
+        System.out.printf(OUTPUT_BENEFIT_FORMAT, OUTPUT_GIFT_BENEFIT, giftPrice);
+    }
+
     private void printDiscount(String discount, Integer amount) {
-        System.out.printf(OUTPUT_BENEFIT, discount, amount);
-    }
-
-    private void printDiscount(Entry<String, Integer> discount) {
-        System.out.printf(OUTPUT_BENEFIT, discount.getKey(), discount.getValue());
+        System.out.printf(OUTPUT_BENEFIT_FORMAT, discount, amount);
     }
 
     public void printBenefitAmount(int amount) {
         System.out.printf(OUTPUT_TOTAL_BENEFIT);
-        System.out.println(amount);
+        System.out.printf(OUTPUT_TOTAL_BENEFIT_FORMAT, amount);
+    }
+
+    public void printDiscountPrice(int price) {
+        System.out.printf(OUTPUT_DISCOUNT_PRICE);
+        System.out.printf(OUTPUT_DISCOUNT_PRICE_FORMAT, price);
     }
 
     public void printBadge(String badgeName) {
         System.out.printf(OUTPUT_BADGE);
         System.out.println(badgeName);
+    }
+
+    private void print(Runnable runnable, Map<String, Integer> values) {
+        if (values.isEmpty()) {
+            System.out.println(OUTPUT_NOTHING);
+            return;
+        }
+        runnable.run();
     }
 }
