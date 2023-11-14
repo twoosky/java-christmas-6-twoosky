ca35e80a7991a8060ccf149540f76c6022c60549
twoosky <lhjae3@naver.com>
2023-11-14 21:45:31 +0900

    refactor: 함수형 인터페이스 사용해 없음 출력 로직 통합

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -4,6 +4,7 @@ import christmas.dto.BenefitDto;
 import christmas.dto.GiftsDto;
 import christmas.dto.OrdersDto;
 import java.util.Map;
+import java.util.function.BooleanSupplier;
 
 public class OutputView {
     public static final String OUTPUT_RESULT = "12월 3일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!";
@@ -17,7 +18,7 @@ public class OutputView {
     public static final String OUTPUT_BENEFIT_FORMAT = "%s: -%,d원%n";
     public static final String OUTPUT_GIFT_BENEFIT = "증정 이벤트";
     public static final String OUTPUT_TOTAL_BENEFIT = "%n<총혜택 금액>%n";
-    public static final String OUTPUT_TOTAL_BENEFIT_FORMAT = "-%,d원%n";
+    public static final String OUTPUT_TOTAL_BENEFIT_FORMAT = "%,d원%n";
     public static final String OUTPUT_DISCOUNT_PRICE = "%n<할인 후 예상 결제 금액>%n";
     public static final String OUTPUT_DISCOUNT_PRICE_FORMAT = "%,d원%n";
     public static final String OUTPUT_BADGE = "%n<12월 이벤트 배지>%n";
@@ -26,11 +27,7 @@ public class OutputView {
     public void printOrders(OrdersDto dto) {
         System.out.println(OUTPUT_RESULT);
         System.out.printf(OUTPUT_ORDER_MENU_LIST);
-        dto.orders().forEach(this::printOrder);
-    }
-
-    private void printOrder(String menu, int quantity) {
-        System.out.printf(OUTPUT_ORDER_MENU, menu, quantity);
+        printValues(OUTPUT_ORDER_MENU, dto.orders());
     }
 
     public void printPrice(int price) {
@@ -40,30 +37,30 @@ public class OutputView {
 
     public void printGifts(GiftsDto dto) {
         System.out.printf(OUTPUT_GIFT_LIST);
-        print(() -> dto.gifts().forEach(this::printGift), dto.gifts());
-    }
-
-    private void printGift(String gift, Integer quantity) {
-        System.out.printf(OUTPUT_GIFT_FORMAT, gift, quantity);
+        Map<String, Integer> gifts = dto.gifts();
+        printOrNone(() -> printValues(OUTPUT_GIFT_FORMAT, gifts), gifts::isEmpty);
     }
 
     public void printBenefit(BenefitDto dto) {
         System.out.printf(OUTPUT_BENEFIT_LIST);
-        print(() -> dto.discounts().forEach(this::printDiscount), dto.discounts());
+        printDiscountBenefit(dto);
         printGiftBenefit(dto.giftPrice());
     }
 
-    private void printGiftBenefit(int giftPrice) {
-        System.out.printf(OUTPUT_BENEFIT_FORMAT, OUTPUT_GIFT_BENEFIT, giftPrice);
+    private void printDiscountBenefit(BenefitDto dto) {
+        Map<String, Integer> discounts = dto.discounts();
+        printOrNone(() -> printValues(OUTPUT_BENEFIT_FORMAT, discounts), discounts::isEmpty);
     }
 
-    private void printDiscount(String discount, Integer amount) {
-        System.out.printf(OUTPUT_BENEFIT_FORMAT, discount, amount);
+    private void printGiftBenefit(int giftPrice) {
+        if (giftPrice != 0) {
+            System.out.printf(OUTPUT_BENEFIT_FORMAT, OUTPUT_GIFT_BENEFIT, giftPrice);
+        }
     }
 
     public void printBenefitAmount(int amount) {
         System.out.printf(OUTPUT_TOTAL_BENEFIT);
-        System.out.printf(OUTPUT_TOTAL_BENEFIT_FORMAT, amount);
+        System.out.printf(OUTPUT_TOTAL_BENEFIT_FORMAT, (amount * -1));
     }
 
     public void printDiscountPrice(int price) {
@@ -76,8 +73,12 @@ public class OutputView {
         System.out.println(badgeName);
     }
 
-    private void print(Runnable runnable, Map<String, Integer> values) {
-        if (values.isEmpty()) {
+    private void printValues(String message, Map<String, Integer> values) {
+        values.forEach((key, value) -> System.out.printf(message, key, value));
+    }
+
+    private void printOrNone(Runnable runnable, BooleanSupplier supplier) {
+        if (supplier.getAsBoolean()) {
             System.out.println(OUTPUT_NOTHING);
             return;
         }
